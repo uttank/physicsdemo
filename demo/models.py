fe3f4a9711f618a2f00b96c1e643a42ea6e59605
uttank <uttank@snu.ac.kr>
2020-01-08 17:33:22 +0900

    edit models.py settings.py

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Demo_number(models.Model):
+    disk_number = models.CharField(max_length=2)
+
+
+class Demo(models.Model):
+    disk_number = models.ForeignKey(Demo_number, on_delete=models.CASCADE)
+    demo_number = models.CharField(max_length=2)
+    title = models.CharField(max_length=50)
+    movie = models.CharField(max_length=50)
+    sub_chapter = models.CharField(max_length=2)
+    hit_count =models.CharField(max_length=10)
+    download_count = models.CharField(max_length=10)
+
+    class Meta:
+        ordering = ('demo_number',)
